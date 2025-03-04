c5a6abaeda13241e1d74be18a7b23b6ef25756af
abdel-elj <abdelrahman.eljamal@uri.edu>
2025-03-05 00:52:50 +0200

    Adding CLI group to cli.py

--- benchtools/cli.py
+++ benchtools/cli.py
@@ -1,2 +1,30 @@
 import click
 
+
+@click.group()
+def cli():
+    """Benchmark Command-line Interface."""
+    pass
+
+@click.command()
+@click.argument('task_name')
+def add_task(task_name):
+    """Creating a new task."""
+    
+    click.echo(result)
+
+
+@click.command()
+@click.argument('task_name')
+def run_task(task_name):
+    """Running a task."""
+    
+    click.echo(result)
+
+
+cli.add_command(add_task)
+cli.add_command(run_task)
+
+
+if __name__ == "__main__":
+    cli()
