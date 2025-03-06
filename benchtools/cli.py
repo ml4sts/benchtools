7630e35934a5af8f96b555181c97b07774c1f9cf
Emory Salaun <emory_salaun@uri.edu>
2025-03-06 17:53:38 -0500

    Generates Folder Structure For Benchmark
    Generates main folder and two subfolders (reports and tasks) for further filling.
    
    Co-Authored-By: abdel-elj <196731936+abdel-elj@users.noreply.github.com>
    Co-Authored-By: nadia-sousa <98436401+nadia-sousa@users.noreply.github.com>

--- benchtools/cli.py
+++ benchtools/cli.py
@@ -1,11 +1,31 @@
 import click
 from task import PromptTask
 from runner import Bench
+import os
 
 @click.group()
 def cli():
     """Benchmark Command-line Interface."""
     pass
+
+@click.command()
+@click.argument('bench_name')
+def generate_demo_bench(bench_name):
+    current_dir = os.getcwd()
+    parent_dir = os.path.abspath(os.path.join(current_dir, ".."))
+    new_folder_path = os.path.join(parent_dir, bench_name)
+    os.makedirs(new_folder_path, exist_ok=True)
+    click.echo(f"Folder '{bench_name}' created at {new_folder_path}")
+
+    tasks_folder = os.path.join(new_folder_path, "Tasks")
+    report_folder = os.path.join(new_folder_path, "Report")
+
+    os.makedirs(tasks_folder, exist_ok=True)
+    os.makedirs(report_folder, exist_ok=True)
+
+    click.echo("Subfolders 'Tasks' and 'Report' created.")
+
+
 
 @click.command()
 @click.argument('task_name')
@@ -25,6 +45,7 @@ def run_task(task_name):
 
 cli.add_command(add_task)
 cli.add_command(run_task)
+cli.add_command(generate_demo_bench)
 
 
 if __name__ == "__main__":
