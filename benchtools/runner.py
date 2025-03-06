d19bb604b9c455bda0244413ee1bb1a97116f7f9
Emory Salaun <emory_salaun@uri.edu>
2025-03-06 15:05:59 -0500

    From_Yaml and minor fixes.
    From_Yaml has been implemented and tested on the test yml file. Minor changes to the imports in runner.py. Initialization of the tasks list for Bench() has been fixed as well.

--- benchtools/runner.py
+++ benchtools/runner.py
@@ -1,16 +1,21 @@
 # module to run benchmarks
 import pandas
-import yaml
+import yaml # requires pyyaml
+from itertools import product
+from task import PromptTask
+from pathlib import Path
 
 class Bench():
     '''
     '''
-    def __init__(dir, target_dir):
+
+
+    def __init__(self, dir, target_dir):
         '''
         '''
         # load tasks from file strucutre and instantiate task objects for each, store those in a list.
         #    loading will 
-        self.tasks
+        self.tasks = []
 
         
 
@@ -30,8 +35,38 @@ class Bench():
 
         return self
 
-    
-    def from_yaml():
-        '''
-        laod from a yaml
-        '''
+
+    def from_yaml(self, yaml_file):
+        """
+        Load tasks from a YAML file and generate PromptTask objects.
+
+        Parameters
+        ----------
+        yaml_file : str
+            Path to the YAML file containing task templates and values.
+
+        Returns
+        -------
+        self : Bench
+            The Bench instance with tasks populated.
+        """
+        with open(yaml_file, 'r') as file:
+            data = yaml.safe_load(file)
+
+        self.tasks = []
+
+        for entry in data:
+            template = entry["template"]  # Extract template
+            values_dict = entry["values"]  # Extract values dictionary
+
+            # Generate all possible value combinations using itertools.product
+            keys = values_dict.keys()
+            value_combinations = product(*values_dict.values())
+
+            # Create a PromptTask for each combination
+            for values in value_combinations:
+                value_mapping = dict(zip(keys, values))  # Pair keys with values
+                filled_prompt = template.format(**value_mapping)  # Format the template
+                self.tasks.append(PromptTask(prompt=filled_prompt))  # Store task
+
+        return self
