fb98657cf88c4622690526d7e76cd28bea64f0b7
AymanBx <ayman_sandouk@uri.edu>
2025-03-06 20:23:59 -0500

    Planning out the run function and the task object creation

--- benchtools/runner.py
+++ benchtools/runner.py
@@ -2,6 +2,7 @@
 import pandas
 import yaml
 import os
+from log_file.py import log_agent_interaction
 
 class Bench():
     '''
@@ -11,12 +12,13 @@ class Bench():
         '''
         # load tasks from file strucutre and instantiate task objects for each, store those in a list.
         #    loading will 
+
         task_folder = os.listdir(dir)
         for file in task_folder:
             if file.endswith("csv"):
-                self.tasks = self.from_txt_csv()
+                self.tasks = self.from_txt_csv(dir)
             elif file.endswith("yml"):
-                self.tasks = self.from_yaml()
+                self.tasks = self.from_yaml(dir)
         # Both functions should have the same type return. porobably should be a list of PRompt_Task
                     
 
@@ -29,7 +31,16 @@ class Bench():
 
         '''
         for task in self.tasks:
-            task.run(model)
+            (prompt, response) = task.run(model)
+            log_agent_interaction(prompt, response)
+            task.score()
+
+
+
+
+
+
+        
 
     # possibly private method? 
     def from_txt_csv():
