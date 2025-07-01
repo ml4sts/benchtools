fb1ba3043e3673d685307ca9c371b68cd9ca9465
AymanBx <ayman_sandouk@uri.edu>
2025-07-01 14:58:03 +0000

    Small modifications and comments

--- benchtools/cli.py
+++ benchtools/cli.py
@@ -1,32 +1,40 @@
 import click
-from task import PromptTask
-from runner import Bench
+# from task import PromptTask
+# from runner import Bench
 import os
 
 @click.group()
 def cli():
-    """Benchmark Command-line Interface."""
+    """
+    BenchTools is a tool that helps researchers set up benchmarks.
+    """
     pass
 
 @click.command()
 @click.argument('bench_name')
 def generate_demo_bench(bench_name):
+    """
+    Generate a demo benchmark
+    """
+
+    # Set up directory for the demo bench
     current_dir = os.getcwd()
     parent_dir = os.path.abspath(os.path.join(current_dir, ".."))
-    new_folder_path = os.path.join(parent_dir, bench_name)
-    os.makedirs(new_folder_path, exist_ok=True)
-    click.echo(f"Folder '{bench_name}' created at {new_folder_path}")
+    # Change this to use the current path or possibly take a path as an argument
+    demo_bench_path = os.path.join(parent_dir, bench_name)
+    os.makedirs(demo_bench_path, exist_ok=True)
 
-    tasks_folder = os.path.join(new_folder_path, "Tasks")
-    report_folder = os.path.join(new_folder_path, "Report")
+    tasks_folder = os.path.join(demo_bench_path, "Tasks")
+    report_folder = os.path.join(demo_bench_path, "Report")
 
     os.makedirs(tasks_folder, exist_ok=True)
     os.makedirs(report_folder, exist_ok=True)
 
+    click.echo(f"Folder '{bench_name}' created at {demo_bench_path}")
     click.echo("Subfolders 'Tasks' and 'Report' created.")
 
 
-
+# What us creating a new task
 @click.command()
 @click.argument('task_name')
 def add_task(task_name):
