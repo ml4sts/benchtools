3d913341bf48378f7f4f43ceb484882eb724177b
AymanBx <ayman_sandouk@uri.edu>
2025-12-06 07:11:27 +0000

    Designer: Finally added modularity so that the bench class can be used either through the CLI or imported into a new code

--- benchtools/runner.py
+++ benchtools/runner.py
@@ -1,52 +1,73 @@
 # module to run benchmarks
 import os
-from pathlib import Path
-from itertools import product
 from benchtools.task import Task
+from benchtools.designer import build_dir, init_repo, create_about, setup_task
 # from log_file.py import log_agent_interaction
 
 
 class Bench():
     '''
     '''
-    def __init__(self, bench_dir):
+    def __init__(self, name, path):
         '''
         '''
         # load tasks from file strucutre and instantiate task objects for each, store those in a list.
         #    loading will 
-        self.directory = bench_dir
+        self.bench_name = name
+        self.bench_path = path
+        self.tasks_folder = os.path.join(self.bench_path, 'benchmarks')
         self.tasks = []
+        self.built = os.path.exists(self.bench_path)
     
-        tasks_folder = os.path.join(bench_dir, "benchmarks")
-        tasks = os.listdir(tasks_folder)
+
+    def build(self, about_text, no_git, new_tasks) -> bool:
+
+        # Create benchmark skeleton 
+        build_dir(self.bench_path)
+
+        # Create about.md
+        create_about(self.bench_name, self.bench_path, about_text)
+
+        # Initialize a git repo
+        if not no_git:
+            init_repo(self.bench_path)
+
+        for task_name, task_path in new_tasks:
+            self.new_task(task_name, task_path)
+
+        self.built = True
+        return self.built
+
+
+    def new_task(self, task_name, task_path):
+        if self.built:
+            self.tasks.append(setup_task(self.tasks_folder, task_name, task_path))
+
+
+    def run(self, model='gemma3', api_url=None):
+        '''
+        '''
+        tasks = os.listdir(self.tasks_folder)
         for task in tasks:
-            content = os.listdir(os.path.join(tasks_folder,task))
+            task_folder = os.path.join(self.tasks_folder,task)
+            content = os.listdir(task_folder)
             for file in content:
                 if file.endswith("csv"):
-                    self.tasks.append(Task('csv', task, os.path.join(tasks_folder,task)))
+                    self.tasks.append(Task('csv', task, task_folder))
                 elif file.endswith("yml"):
-                    self.tasks.append(Task('yml', task, os.path.join(tasks_folder,task,file)))
+                    self.tasks.append(Task('yml', task, os.path.join(self.task_folder,file)))
                     
         for task in self.tasks:
+            print("\n\n\n")
             name, prompts, answers = task.name, task.sub_tasks, task.answers
             print("Task: " + name)
             print("Prompts: ", end='')
             print(prompts)
             print("Answers: ", end='')
             print(answers)
+            task.run(model, api_url)
             print("Responses: ", end='')
-            task.run("gemma3")
             print(task.responses)
 
-
-        
-
-
-    def run(self, model, api_url=None):
-        '''
-        '''
-        for task in self.tasks:
-            task.run(model, api_url)
             # log_agent_interaction(prompt, response)
             # task.score()
-            print(task.responses)
