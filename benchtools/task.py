b8d7f622e6f4da266c74a43f7cd270db9bb7778a
Sarah M Brown <brownsarahm@uri.edu>
2026-02-14 14:56:37 -0500

    rename, start class methods, and move runner all to run method to make the task objects more usable in design

--- benchtools/task.py
+++ benchtools/task.py
@@ -16,56 +16,110 @@ class Task:
     """
 
     def __init__(
-        self, data_type, task_name, path, log_path, scoring_function=None, reference=None, runner_type="ollama"
+        self, task_name, prompt, reference, scoring_function=None, prompt_variants = None, 
     ):
         """
-        init a task object
+        init a task object from a prompt and reference, and a scoring function. If no scoring function is provided, defaults to exact match.
 
         Parameters
         ----------
         dir : string or path
             directory containing the task assets
-
+        prompt: string
+            prompt for task or overall description 
         scoring_function : function handle or string
             if string, must be name of built in eval function provided here
-        reference: string or number
+        reference: string or number or list of 
             solution that will be passed with the model answer to the scoring function
-        runner_type: string {ollama}
-            the way the runner should be called,
-            solution that will be passed with the model answer
-        runner_type: string {ollama,openai}
-            define which runner should be used for the task.
-            to use the Ollama runner, the script expects the model to be installed, and `ollama serve` running on localhost:11434
-            to use OpenAI runner, you must have an API key set in your OPENAI_API_KEY environment variable
         """
         self.name = task_name
-        self.log_path = log_path
-        self.sub_tasks = []
-        self.answers = []
-        match data_type:
-            case 'csv':
-                prompts, answers = from_txt_csv(path)
-                self.sub_tasks += prompts
-                self.answers += answers
-            case 'yml':
-                prompts, answers = from_yaml(path)
-                self.sub_tasks += prompts
-                self.answers += answers
 
+        if prompt_variants:
+            self.sub_tasks = prompt_variants
+            self.description = prompt 
+        else:
+            self.sub_tasks = [prompt]
+            self.description = f"a basic prompt task with: {prompt}"
+
+        
+        self.reference = reference
 
         if type(scoring_function) is str:
             self.scoring_function = scoring_function[scoring_function]
         else:
             self.scoring_function = scoring_function
 
-        self.reference = reference
-        self.runner_type = runner_type
-        self.responses = []
+        
+        # self.responses = []
 
-        self.logger = init_logger(self.log_path, self.name)
+        # self.logger = init_logger(self.log_path, self.name)
 
+    
 
-    def run(self, model, api_url=None):
+    @classmethod
+    def from_txt_csv(cls, task_name, task_folder):
+        '''
+        load a template from txt and create task objects for each row of a csv
+
+        folder must contain a task.txt file with the template, and a values.csv file with the values to fill in the template, and the reference answers. The csv should be structured as follows:
+        '''
+        # using pandas to load the csv is easy, then use python string formatting to set up the final prompt to apss to the task constructor
+        prompt = ""
+        with open(os.path.join(task_folder, "task.txt"), "r") as f:
+            prompt = f.read()
+
+        value_answer_df = pandas.read_csv(os.path.join(task_folder, "values.csv"))
+        # answers = pandas.read_csv(os.path.join(task_folder, "results"))
+        storedTasks = []
+        storedAnswers = []
+        for x in range(len(value_answer_df)):
+            processed_prompt = prompt.replace("{a}", str(value_answer_df.iloc[x,0]))
+            processed_prompt = processed_prompt.replace("{b}", str(value_answer_df.iloc[x, 1]))
+            storedTasks.append(processed_prompt)
+            # print("Prompt: "+ processed_prompt) # Debugging
+            storedAnswers.append(str(value_answer_df.iloc[x, 2]))
+        
+        description = f"a template based task with template: {prompt} and values like:\n\n {value_answer_df.head().to_markdown()}"
+
+        return cls(task_name, prompt_variants = storedTasks, reference=storedAnswers)
+
+        
+
+    @classmethod
+    def from_yaml(yaml_file):
+        """
+        Load tasks from a YAML file and generate PromptTask objects.
+        Parameters
+        ----------
+        yaml_file : str
+            Path to the YAML file containing task templates and values.
+        Returns
+        -------
+        self : Bench
+            The Bench instance with tasks populated.
+        """
+        with open(yaml_file, 'r') as file:
+            data = yaml.safe_load(file)
+        storedTasks = []
+        storedAnswers = []
+        for sub_task in data:
+            template = sub_task["template"]  # Extract template
+            values_dict = sub_task["values"]  # Extract values dictionary
+            answers = sub_task["result"]
+            # Generate all possible value combinations using itertools.product
+            keys = values_dict.keys()
+            value_combinations = zip(*values_dict.values())
+            # Create a PromptTask for each combination
+            for values in value_combinations:
+                value_mapping = dict(zip(keys, values))  # Pair keys with values
+                filled_prompt = template.format(**value_mapping)  # Format the template
+                # print("Prompt: "+ filled_prompt) # Debugging
+                storedTasks.append(filled_prompt)  # Store task
+            for answer in answers:
+                storedAnswers.append(answer)
+        return (storedTasks, storedAnswers)
+
+    def run(self, model,runner_type="ollama", api_url=None):
         """
         run the task on the model
 
@@ -75,12 +129,16 @@ class Task:
             the model to run the task on
         api_url : string
             the url of the api to use for the task
+        runner_type: string {ollama,openai}
+            define which runner should be used for the task.
+            to use the Ollama runner, the script expects the model to be installed, and `ollama serve` running on localhost:11434
+            to use OpenAI runner, you must have an API key set in your OPENAI_API_KEY environment variable
         """
 
         for sub_task in self.sub_tasks:
             # print(sub_task)
 
-            match self.runner_type:
+            match runner_type:
                 case "ollama":
                     response: ChatResponse = chat(model=model, messages=[
                         {
@@ -142,61 +200,3 @@ class Task:
 # additional classes for other types of tasks
 
 # likely an agent task that can pass environment assets
-
-
-# possibly private method?  # TODO: Fix csv indexing?
-def from_txt_csv(task_folder):
-    '''
-    load a template from txt and create task objects for each row of a csv
-    '''
-    # using pandas to load the csv is easy, then use python string formatting to set up the final prompt to apss to the task constructor
-    prompt = ""
-    with open(os.path.join(task_folder, "task.txt"), "r") as f:
-        prompt = f.read()
-
-    csvFile = pandas.read_csv(os.path.join(task_folder, "values.csv"))
-    # answers = pandas.read_csv(os.path.join(task_folder, "results"))
-    storedTasks = []
-    storedAnswers = []
-    for x in range(len(csvFile)):
-        processed_prompt = prompt.replace("{a}", str(csvFile.iloc[x,0]))
-        processed_prompt = processed_prompt.replace("{b}", str(csvFile.iloc[x, 1]))
-        storedTasks.append(processed_prompt)
-        # print("Prompt: "+ processed_prompt) # Debugging
-        storedAnswers.append(str(csvFile.iloc[x, 2]))
-    
-    return (storedTasks, storedAnswers)
-
-
-def from_yaml(yaml_file):
-    """
-    Load tasks from a YAML file and generate PromptTask objects.
-    Parameters
-    ----------
-    yaml_file : str
-        Path to the YAML file containing task templates and values.
-    Returns
-    -------
-    self : Bench
-        The Bench instance with tasks populated.
-    """
-    with open(yaml_file, 'r') as file:
-        data = yaml.safe_load(file)
-    storedTasks = []
-    storedAnswers = []
-    for sub_task in data:
-        template = sub_task["template"]  # Extract template
-        values_dict = sub_task["values"]  # Extract values dictionary
-        answers = sub_task["result"]
-        # Generate all possible value combinations using itertools.product
-        keys = values_dict.keys()
-        value_combinations = zip(*values_dict.values())
-        # Create a PromptTask for each combination
-        for values in value_combinations:
-            value_mapping = dict(zip(keys, values))  # Pair keys with values
-            filled_prompt = template.format(**value_mapping)  # Format the template
-            # print("Prompt: "+ filled_prompt) # Debugging
-            storedTasks.append(filled_prompt)  # Store task
-        for answer in answers:
-            storedAnswers.append(answer)
-    return (storedTasks, storedAnswers)
