0d69cd3ce84af70ac61d6904cd350c2cf9969384
AymanBx <ayman_sandouk@uri.edu>
2026-02-17 07:40:06 +0000

    BenchRunner: Changed docstring

--- benchtools/runner.py
+++ benchtools/runner.py
@@ -5,22 +5,23 @@ from benchtools.task import Task
 # from log_file.py import log_agent_interaction
 
 
+# possibly resurected for batch runs? 
 class BenchRunner():
     '''
-    unused currenlty; possibly resurected for batch runs? 
+    A BenchRunner holds information about how a task is going to be run. 
     '''
 
     def init(self, runner_type='ollama', model='gemma3', api_url=None):
         '''
-        Run the benchmark by running each task in the benchmark and logging the interactions.
-        Parameters:
+        The constructor for BenchRunner will have default values for all attributes to have a full default runner ready to be used for running any task.
+        P.S. Requires Ollama to be installed and running on your machine.
         -----------
-        type: 
-            The used engine for running an LLM
+        runner_type: str default 'ollama'
+            The used engine for running an LLM. Default is ollama that will need to be installed and running on your machine
         model: str default 'gemma3'
-            The name of the model to use for running the tasks. Default is 'gemma3'.
+            The name of the LLM to use for running the tasks. Default is 'gemma3'. P.S. Will need to have the model downloaded locally if using ollama
         api_url: str
-            The URL of the API to use for running the tasks. If None, the default API
+            The URL of the API to use for accessing an LLM. If None, the default API will be http://localhost:11434 as this is used by ollama by default
         '''
 
         self.runner_type = runner_type
